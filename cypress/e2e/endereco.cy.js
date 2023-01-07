d80d5365fa6bc98d901c77ec47076ec527c0d41d
Eliezer Castro <eliezergino@gmail.com>
2023-01-07 11:12:05 -0300

    usando arquivos de dados

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,9 @@
 /// <reference types="cypress" />
+//import EnderecoPage from '../fixtures/endereco.json'
+const dadosEndereco = require('../fixtures/endereco.json')
+
+
+import EnderecoPage from '../support/pages-objects/endereco.page'
 
 describe('funcionalidade de endereços - faturamento e entrega', () => {
   beforeEach(() => {
@@ -6,11 +11,28 @@ describe('funcionalidade de endereços - faturamento e entrega', () => {
     cy.fixture('perfil').then(dados =>{
       cy.login(dados.usuario, dados.senha)
     })
-    
   });
   
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-      
+      EnderecoPage.editarEnderecoFaturamento('Eliezer', 'Castro', 'Kothe', 'Brasil', 'Rua Maria Alice', '711', 'Gravatá', 'Pernambuco', '55641520', '+5563995124511', 'faturamento@mail.com')
+      cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+      EnderecoPage.editarEnderecoFaturamento(
+        dadosEndereco[1].nome, 
+        dadosEndereco[1].sobrenome, 
+        dadosEndereco[1].empresa,
+        dadosEndereco[1].pais,
+        dadosEndereco[1].endereco,
+        dadosEndereco[1].numero,
+        dadosEndereco[1].cidade,
+        dadosEndereco[1].estado,
+        dadosEndereco[1].cep,
+        dadosEndereco[1].telefone,
+        dadosEndereco[1].email
+        )
+      cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
     
 });
